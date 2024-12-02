b0f5f113f7be330f9db0d45d3db9864e5a4a3df8
KelvinWong3740 <hinhin3740@gmail.com>
2024-12-02 19:06:35 +0800

    added pitches.h

---
+++ pitches.h
@@ -0,0 +1,8 @@
+#define NOTE_E5 659.25
+#define NOTE_F5 698.5
+#define NOTE_G5 784
+#define NOTE_GS5 830.6
+#define NOTE_A5 880
+#define NOTE_B5 987.76
+#define NOTE_C6 1046.5
+#define NOTE_CS6 1108.73
